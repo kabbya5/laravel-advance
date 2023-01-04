9829ad22125f1aedaca03d3d7253ec42002ddaeb
kabbya <kabbya44@gmil.com>
2023-01-04 14:50:55 +0600

    learnt about event listening and mail

---
+++ app/Mail/UserCreateNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class UserCreateNotification extends Mailable 
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'User Create Notification',
+            from: 'admin@gmail.com',
+            // replyTo:'kabbya@gmail.com',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'event.mail',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [
+            // Attachment::fromPath('/public/test')
+            // ->as('test.pdf')
+            // ->withMime('application/pdf'),
+        ];
+    }
+}
